f3ba30417150c783f0db2546945c43aec6816a8d
belenkanas <maria.kanas@correo.ucu.edu.uy>
2025-10-30 09:59:22 -0300

    conexion en pyhton

--- src/obligatorio.py
+++ src/obligatorio.py
@@ -1,17 +1,34 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(
-    user='root',
-    password='rootpassword',
-    host='127.0.0.1',
-    database='obligatorio')
+def conexion():
+  return mysql.connector.connect(user='root', 
+                                 password='rootpassword',
+                                 host='127.0.0.1',
+                                 database='obligatorio')
 
-cursor = cnx.cursor()
-query = ("SELECT * FROM Usuario")
 
-cursor.execute(query)
+def listar_participantes():
+  cnx = conexion()
+  cursor = cnx.cursor()
 
-for el in cursor:
-    print(el)
+  query = ("SELECT * FROM participante")
 
-cnx.close()
+  cursor.execute(query)
+  print("\n--- Lista de Usuarios ---")
+  for (ci, nombre, apellido, email) in cursor:
+        print(f"{ci} - {nombre} {apellido} ({email})")
+  cursor.close()
+  cnx.close()
+
+def menu():
+    while True:
+        print("\nSeleccione una opción:")
+        print("1. Listar Participantes")
+        opcion = input("Ingrese el número de la opción deseada: ")
+        if opcion == '1':
+            listar_participantes()
+        else:
+                print("Opción inválida, intenta de nuevo.")
+
+if __name__ == "__main__":
+    menu()
