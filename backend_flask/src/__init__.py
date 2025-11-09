82060e88e3ce02206e6d946c7c4c07ced791ef53
belenkanas <maria.kanas@correo.ucu.edu.uy>
2025-11-08 23:26:25 -0300

    import de endpoint en __init__

--- backend_flask/src/__init__.py
+++ backend_flask/src/__init__.py
@@ -4,6 +4,9 @@ from endpoints.login_bp import login_bp
 from endpoints.participante_bp import participante_bp
 from endpoints.programa_academico_bp import programa_academico_bp
 from endpoints.participante_programa_academico_bp import participante_programa_academico_bp
+from endpoints.reserva_bp import reserva_bp
+from endpoints.reserva_participante_bp import reserva_participante_bp
+from endpoints.sancion_participante_bp import sanciones_bp
 
 def create_app():
      app = Flask(__name__)
@@ -12,7 +15,9 @@ def create_app():
      app.register_blueprint(participante_bp)
      app.register_blueprint(programa_academico_bp)
      app.register_blueprint(participante_programa_academico_bp)
-
+     app.register_blueprint(reserva_bp)
+     app.register_blueprint(reserva_participante_bp)
+     app.register_blueprint(sanciones_bp)
      return app
 
 if __name__ == '__main__':
