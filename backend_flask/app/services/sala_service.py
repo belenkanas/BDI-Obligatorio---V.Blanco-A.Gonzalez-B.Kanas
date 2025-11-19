eb75687c44b6c49b9482ce23ea0287ce5882630a
andreagonzalezucu <andrea.gonzalezr@correo.ucu.edu.uy>
2025-11-19 08:05:22 -0300

    salaserv

--- backend_flask/app/services/sala_service.py
+++ backend_flask/app/services/sala_service.py
@@ -4,12 +4,23 @@ def listar_salas():
     conn = conexion()
     cursor = conn.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM sala")
+    cursor.execute("""
+        SELECT 
+            s.id_sala,
+            s.nombre_sala,
+            s.capacidad,
+            s.tipo_sala,
+            e.id_edificio,
+            e.nombre_edificio
+        FROM sala s
+        JOIN edificio e ON s.id_edificio = e.id_edificio
+    """)
 
     salas = cursor.fetchall()
     conn.close()
 
     return salas
+
 
 
 def obtener_sala(nombre_sala):
@@ -24,31 +35,36 @@ def obtener_sala(nombre_sala):
     return sala
 
 
-def agregar_sala(nombre_sala, edificio, capacidad, tipo_sala):
+def agregar_sala(nombre_sala, id_edificio, capacidad, tipo_sala):
     conn = conexion()
     cursor = conn.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM sala WHERE nombre_sala = %s AND edificio = %s",
-                   (nombre_sala, edificio))
+    cursor.execute("""
+        SELECT * FROM sala 
+        WHERE nombre_sala = %s AND id_edificio = %s
+    """, (nombre_sala, id_edificio))
 
     if cursor.fetchone():
         conn.close()
         return None, "La sala ya existe en ese edificio"
 
     cursor.execute("""
-        INSERT INTO sala (nombre_sala, edificio, capacidad, tipo_sala)
+        INSERT INTO sala (nombre_sala, id_edificio, capacidad, tipo_sala)
         VALUES (%s, %s, %s, %s)
-    """, (nombre_sala, edificio, capacidad, tipo_sala))
+    """, (nombre_sala, id_edificio, capacidad, tipo_sala))
 
     conn.commit()
+    nuevo_id = cursor.lastrowid
     conn.close()
 
     return {
+        "id_sala": nuevo_id,
         "nombre_sala": nombre_sala,
-        "edificio": edificio,
+        "id_edificio": id_edificio,
         "capacidad": capacidad,
         "tipo_sala": tipo_sala
     }, "Sala creada exitosamente"
+
 
 
 def eliminar_sala(nombre_sala):
